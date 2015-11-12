d94e506a721b0a6c718ed0e140fb2bb0b95ff7bc
陈淑飞 <chensf@lianchuang.com>
2015-11-12 16:31:02 +0800

    测试...

---
+++ jitpacklibdemo/src/main/java/com/test/jitpacklibdemo/HelloWorldTest.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2015. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com.test.jitpacklibdemo;
+
+/**
+ * <br /> author: chenshufei
+ * <br /> date: 15/11/12
+ * <br /> email: chenshufei2@sina.com
+ */
+public class HelloWorldTest {
+
+    /**
+     * num1 + num2 的和
+     * @param num1
+     * @param num2
+     * @return
+     */
+    public int add(int num1,int num2){
+        return num1 + num2;
+    }
+
+    /**
+     * num1 - num2 的值
+     * @param num1
+     * @param num2
+     * @return
+     */
+    public int sub(int num1,int num2){
+        return num1 - num2;
+    }
+
+}
+
